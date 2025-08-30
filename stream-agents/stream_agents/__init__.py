4d65b127c0d4d55bd76d6f7fba70005091b09a4d
Thierry Schellenbach <thierry@getstream.io>
2025-08-29 19:18:59 -0600

    wip

--- stream-agents/stream_agents/__init__.py
+++ stream-agents/stream_agents/__init__.py
@@ -3,3 +3,10 @@ from getstream import Stream as Stream
 from stream_agents.edge import StreamEdge as StreamEdge
 from stream_agents.cli import start_dispatcher as start_dispatcher
 from stream_agents.utils import open_demo as open_demo
+
+
+from importlib import metadata
+try:
+    __version__ = metadata.version("stream_agents")
+except metadata.PackageNotFoundError:  # editable/checkout scenarios
+    __version__ = "0.0.0"

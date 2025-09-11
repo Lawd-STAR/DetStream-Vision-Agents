dfb55bb2c9873234f433f463c36c5851357846ea
Neevash Ramdial (Nash) <mail@neevash.dev>
2025-09-11 14:57:29 -0600

    Use openai for main

--- examples/01_simple_agent_example/simple_agent_example.py
+++ examples/01_simple_agent_example/simple_agent_example.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 from dotenv import load_dotenv
 
-from stream_agents.plugins import elevenlabs, deepgram, xai
+from stream_agents.plugins import elevenlabs, deepgram, openai
 from stream_agents.core import agents, edge, cli
 from getstream import Stream
 
@@ -23,9 +23,7 @@ async def start_agent() -> None:
         edge=edge.StreamEdge(),  # low latency edge. clients for React, iOS, Android, RN, Flutter etc.
         agent_user=agent_user,  # the user object for the agent (name, image etc)
         instructions="You're a voice AI assistant. Keep responses short and conversational. Don't use special characters or formatting. Be friendly and helpful.",
-        # tts, llm, stt more. see the realtime example for sts
-        #llm=openai.LLM(model="gpt-4o-mini"),
-        llm=xai.LLM(model="grok-4"),
+        llm=openai.LLM(model="gpt-4o-mini"),
         tts=elevenlabs.TTS(),
         stt=deepgram.STT(),
         processors=[],  # processors can fetch extra data, check images/audio data or transform video
@@ -40,13 +38,7 @@ async def start_agent() -> None:
     # Have the agent join the call/room
     with await agent.join(call):
         # Example 1: standardized simple response (aggregates delta/done)
-        await agent.llm.simple_response("Introduce yourself as the mighty Grok capable of streaming AI responses")
-
-        # Example 2: streaming response
-        await agent.llm.create_response(
-            input="You are a conversational Grok",
-            stream=True
-        )
+        await agent.llm.simple_response("Please say verbatim: 'this is a test of the OpenAI realtime api.'.")
 
         await agent.finish()  # run till the call ends
 

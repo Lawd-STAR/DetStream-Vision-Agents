32de1335352d2a3b7f7d863d0e687c2c3876653b
Neevash Ramdial (Nash) <mail@neevash.dev>
2025-09-16 21:16:38 -0600

    Feat/cluely setup (#32)
    * WIP XAI README
    
    * Show streaming support in Readme
    
    * cleanup
    
    * Basic cleanup

--- examples/02_cluely_example/cluely_example.py
+++ examples/02_cluely_example/cluely_example.py
@@ -5,57 +5,43 @@ from uuid import uuid4
 from dotenv import load_dotenv
 from getstream import Stream
 from getstream.models import UserRequest
-from stream_agents.plugins import deepgram, elevenlabs
-from stream_agents.core.processors import YOLOPoseProcessor
+from stream_agents.plugins import deepgram, elevenlabs, openai
+from stream_agents.core import processors, edge, agents, cli
 
-# TODO: imports are not nice
-from stream_agents.core.turn_detection import FalTurnDetection
+# Main feats:
+# 1. API endpoints to create a sessions, end session
+# 2. Generate 2 - 4 sentiments for new chat messages
 
-from stream_agents.core.edge.edge_transport import StreamEdge
-from stream_agents.core.utils import open_demo
-from stream_agents.core.llm import OpenAILLM
-from stream_agents.core.agents.agents import Agent
-from stream_agents.core.cli import start_dispatcher
+load_dotenv()
 
 
 async def main() -> None:
     """Create a simple agent and join a call."""
-
-    load_dotenv()
-
-    # TODO this user creation flow is ugly.
     agent_user = UserRequest(id=str(uuid4()), name="My happy AI friend")
     client = Stream.from_env()
     client.upsert_users(UserRequest(id=agent_user.id, name=agent_user.name))
 
-    # Create the agent
-    turn_detection = FalTurnDetection(
-        buffer_duration=3.0,  # Process 3 seconds of audio at a time
-        confidence_threshold=0.7,  # Higher threshold for more confident detections
-    )
-
     # TODO: LLM class
-    agent = Agent(
-        edge=StreamEdge(),  # low latency edge. clients for React, iOS, Android, RN, Flutter etc.
+    agent = agents.Agent(
+        edge=edge.StreamEdge(),  # low latency edge. clients for React, iOS, Android, RN, Flutter etc.
         agent_user=agent_user,  # the user name etc for the agent
         instructions="You're a voice AI assistant. Keep responses short and conversational. Don't use special characters or formatting. Be friendly and helpful.",
         # tts, llm, stt more. see the realtime example for sts
-        llm=OpenAILLM(
+        llm=openai.LLM(
             model="gpt-4o",
+
         ),
         tts=elevenlabs.TTS(),
         stt=deepgram.STT(),
-        # turn keeping
-        turn_detection=turn_detection,
         # processors can fetch extra data, check images/audio data or transform video
-        processors=[YOLOPoseProcessor()],
+        processors=[processors.YOLOPoseProcessor()],
     )
 
     try:
         # Join the call - this is the main functionality we're demonstrating
         call = client.video.call("default", str(uuid4()))
         # Open the demo env
-        open_demo(call)
+        agent.edge.open_demo(call)
 
         # have the agent join a call/room
         await agent.join(call)
@@ -68,4 +54,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(start_dispatcher(main))
+    asyncio.run(cli.start_dispatcher(main))

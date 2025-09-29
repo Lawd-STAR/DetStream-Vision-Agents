460c5bf910790daaacbd021e40053c6198f74acf
Thierry Schellenbach <thierry@getstream.io>
2025-09-29 16:40:14 -0600

    watch the right track

--- examples/02_golf_coach_example/golf_coach_example.py
+++ examples/02_golf_coach_example/golf_coach_example.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 from dotenv import load_dotenv
 
-from stream_agents.plugins import gemini, getstream, ultralytics
+from stream_agents.plugins import gemini, getstream, ultralytics, openai
 from stream_agents.core.agents import Agent
 from stream_agents.core.cli import start_dispatcher
 from getstream import Stream, AsyncStream
@@ -23,8 +23,8 @@ async def start_agent() -> None:
         edge=getstream.Edge(),
         agent_user=agent_user,
         instructions="Read @golf_coach.md",
-        llm=gemini.Realtime(fps=1),
-        #llm=openai.Realtime(fps=5), # Careful with FPS can get expensive
+        #llm=gemini.Realtime(fps=1),
+        llm=openai.Realtime(fps=1), # Careful with FPS can get expensive
         processors=[ultralytics.YOLOPoseProcessor(model_path="yolo11n-pose.pt")],
     )
 

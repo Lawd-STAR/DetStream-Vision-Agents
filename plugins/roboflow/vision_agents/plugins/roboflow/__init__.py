ce4b10d9c7f13e28ef663b4b1e803557f66de0cc
Max Kahan <max.kahan@getstream.io>
2025-10-07 12:14:54 +0200

    re add files

---
+++ plugins/roboflow/vision_agents/plugins/roboflow/__init__.py
@@ -0,0 +1,12 @@
+"""
+Roboflow plugin for stream-agents.
+
+This plugin provides Roboflow object detection capabilities.
+"""
+
+from .roboflow_processor import RoboflowProcessor
+
+
+__all__ = [
+    "RoboflowProcessor",
+]
